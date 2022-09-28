117ed67f1bd7f398e329b6989cb90dbfda11f379
Elvis <elviscruz45@gmail.com>
2022-09-28 16:41:40 -0500

    finished

--- src/main.js
+++ src/main.js
@@ -7,15 +7,19 @@ const api=axios.create({
     params:{
         "api_key": API_KEY,
 
+
     }
 })
 
 async function getTrendingMoviesPreview(){
     const {data}=await api("trending/movie/day")
     const movies =data.results
+    trendingMoviesPreviewList.innerHTML=""
+
+
+
 
     movies.forEach(movie=>{
-        const trendingPreviewsMoviesContainer=document.querySelector("#trendingPreview .trendingPreview-movieList")
 
 
         const movieContainer=document.createElement("div")
@@ -28,7 +32,7 @@ async function getTrendingMoviesPreview(){
 
 
         movieContainer.appendChild(movieImg);
-        trendingPreviewsMoviesContainer.appendChild(movieContainer)
+        trendingMoviesPreviewList.appendChild(movieContainer)
     })
     
 }
@@ -38,9 +42,12 @@ async function getCategoriesPreview(){
     const {data}=await api("genre/movie/list")
     const categories =data.genres
 
-    categories.forEach(category=>{
-        const previewCategoriesContainer=document.querySelector("#categoriesPreview .categoriesPreview-list")
+    categoriesPreviewList.innerHTML=""
 
+
+
+
+    categories.forEach(category=>{
 
         const categoryContainer=document.createElement("div")
         categoryContainer.classList.add("category-container")
@@ -48,16 +55,44 @@ async function getCategoriesPreview(){
         const categoryTitle=document.createElement("h3")
         categoryTitle.classList.add("category-title")
         categoryTitle.setAttribute("id", "id"+ category.id)
+        categoryTitle.addEventListener("click",()=>{location.hash=`#category=${category.id}-${category.name}`})
+
+
         const categoryTitleText=document.createTextNode(category.name)
 
 
         categoryTitle.appendChild(categoryTitleText);
         categoryContainer.appendChild(categoryTitle);
-        previewCategoriesContainer.appendChild(categoryContainer);
+        categoriesPreviewList.appendChild(categoryContainer);
 
     })
     
 }
-getTrendingMoviesPreview()
 
-getCategoriesPreview()
+
+async function getMoviesByCategory(id){
+    const {data}=await api("discover/movie",{
+        params:{
+            with_genres:id
+        }
+    })
+    const movies =data.results
+    genericSection.innerHTML=""
+
+    movies.forEach(movie=>{
+
+
+        const movieContainer=document.createElement("div")
+        movieContainer.classList.add("movie-container")
+
+        const movieImg=document.createElement("img")
+        movieImg.classList.add("movie-img")
+        movieImg.setAttribute("alt",movie.title)
+        movieImg.setAttribute("src","https://image.tmdb.org/t/p/w300/"+movie.poster_path,)
+
+
+        movieContainer.appendChild(movieImg);
+        genericSection.appendChild(movieContainer)
+    })
+    
+}
